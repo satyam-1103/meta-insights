666ffb395af3ce5134c7f9fe4160f5915efc0a20
Satyam Srivastava <satyamsrivastava052@gmail.com>
2025-01-14 16:06:28 +0530

    added &apos; on login page

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -113,7 +113,7 @@ const Page = () => {
 
         {/* Footer Links */}
         <p className="text-center text-sm text-gray-600 mt-6">
-          Don't have an account?{" "}
+          Don &apos; t have an account?{" "}
           <Link
             href="/auth/register"
             className="text-blue-500 hover:underline font-medium"
